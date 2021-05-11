255392eafee8a9506222ec263109b830e06f774a
Alexander Hoffmann <alex.otto.hoffmann@gmail.com>
2021-05-11 23:39:21 +0200

    Picture implementation and progress on project overview page

--- resources/javascript/eventListeners.js
+++ resources/javascript/eventListeners.js
@@ -1,14 +1,17 @@
 const btnTeamCalendar = document.getElementById('open-teamcalendar');
 const btnTeamStats = document.getElementById('open-teamstats');
 const btnPViewer = document.getElementById('open-pviewer');
+const btnSubmitFilters1 = document.getElementById('submitfilters1')
 
 let inputCountry = document.getElementById('countryfilter');
 let inputSector = document.getElementById('sectorfilter');
 let inputProject = document.getElementById('projectfilter');
-let arrCurrentProjectList = arrAllProjects;
+//let arrCurrentProjectList = arrAllProjects;
+//let arrCurrentSelectedProject =[];
 
 let pvTitleH1 = document.querySelector('#pv-title h1');
 let pvTitleH2 = document.querySelector('#pv-title h2');
+let displayIMGElement = document.getElementById('pv_projectimage');
 
 function showTeamCalendar(event) {
     document.querySelector('.welcomepage').style.display = 'none';
@@ -58,34 +61,54 @@ function updateProjectFilter() {
     arrAllowedProjects = [... new Set(arrAllProjects.filter(element => (element._country === selectedCountry) && (element._team === selectedSector)).map(element => element._name))];
   }
 
-  arrCurrentProjectList = arrAllowedProjects;
+  
+  /*pvTitleH1.innerHTML = [... new Set(arrAllProjects.map(element => element._name))][0];*/
+
+  arrCurrentProjectList = arrAllowedProjects;  
   
   if(pviewerBoardVisibility.style.display !== "block"){
     arrAllowedProjects.unshift('All');
   }
 
+  //arrAllowedProjects.unshift('All');
+
   arrAllowedProjects.forEach(function(element){
     let newEntry = document.createElement('option');
     newEntry.text = element
-    inputProject .add(newEntry);
+    inputProject.add(newEntry);
   })
 
+  updatePViewer();
   
 };
 
 function updatePViewer(){
-  let selectedProject = document.querySelector('#countryfilter option').innerHTML
-  if(selectedProject === 'All'){
-    selectedProject = arrCurrentProjectList[0]._name;
-  }
+  let selectedProject = arrAllProjects.filter(element => element._name === inputProject.value);
+  let selectedProjectDesc = arrProjectDescriptions.filter(element => element._name === inputProject.value);
+  let selectedProjectImg = arrImages.filter(element => element._name === inputProject.value);
+  
 
-  pvTitleH1.innerHTML = [... new Set(arrAllProjects.filter(element => element._name === selectedProject).map(element => element._name))][0];
+  pvTitleH1.innerHTML = [... new Set(selectedProject.map(element => element._name))];
+  pvTitleH2.innerHTML = [... new Set(selectedProject.map(element => element._eng))];
+  document.getElementById('pv-td-bmz-inv').innerHTML = selectedProject.filter(element => element._invbm === 'inv').map(element => element._bmz);
+  document.getElementById('pv-td-bmz-bm').innerHTML = selectedProject.filter(element => element._invbm === 'bm').map(element => element._bmz);
+  document.getElementById('pv-td-inpro-inv').innerHTML = selectedProject.filter(element => element._invbm === 'inv').map(element => element._inpro);
+  document.getElementById('pv-td-inpro-bm').innerHTML = selectedProject.filter(element => element._invbm === 'bm').map(element => element._inpro);
+  
+  document.getElementById('pv-td-inv').innerHTML = selectedProject.filter(element => element._invbm === 'inv').map(element => element._measure).reduce((a, b) => a + b, 0).toLocaleString('de', {style: 'currency', currency: 'EUR'});
+  document.getElementById('pv-td-bm').innerHTML = selectedProject.filter(element => element._invbm === 'bm').map(element => element._measure).reduce((a, b) => a + b, 0).toLocaleString('de', {style: 'currency', currency: 'EUR'});
+  document.getElementById('pv-td-ve').innerHTML = selectedProject.map(element => element._ve).reduce((a, b) => a + b, 0).toLocaleString('de', {style: 'currency', currency: 'EUR'});
+  document.getElementById('pv-td-sbf').innerHTML = selectedProject.map(element => element._sbf).reduce((a, b) => a + b, 0).toLocaleString('de', {style: 'currency', currency: 'EUR'});
+  document.getElementById('pv-description').innerHTML = selectedProjectDesc[0]._desc;
+
+  displayIMGElement.src = selectedProjectImg[0]._source;
+  //document.getElementById('pv_projectimage').alt = selectedProjectImg[0]._alt;
 }
 
 
 inputCountry.addEventListener('input', updateProjectFilter);
 inputSector.addEventListener('input', updateProjectFilter);
-//inputCountry.addEventListener('input', updatePViewer);
+inputProject.addEventListener('input', updatePViewer);
 
 btnTeamCalendar.addEventListener('click', showTeamCalendar);
 btnTeamStats.addEventListener('click', showTeamStats);
@@ -95,6 +118,6 @@ btnTeamCalendar.addEventListener('click', updateProjectFilter);
 btnTeamStats.addEventListener('click', updateProjectFilter);
 btnPViewer.addEventListener('click', updateProjectFilter);
 
+//btnSubmitFilters1.addEventListener('click', updatePViewer)
 
-
-btnPViewer.addEventListener('click', updatePViewer);
+//btnPViewer.addEventListener('click', updatePViewer);
--- resources/javascript/objects.js
+++ resources/javascript/objects.js
@@ -7,7 +7,10 @@ const arrAllProjects =
     _inpro: '54321',
     _invbm: 'inv',
     _country: 'Germany',
-    _team: 'European Fruits'
+    _team: 'Fruits',
+    _measure: 100000000,
+    _ve: 3000000,
+    _sbf: 25000,
   },
   
   {
@@ -17,7 +20,10 @@ const arrAllProjects =
     _inpro: '12345',
     _invbm: 'bm',
     _country: 'Germany',
-    _team: 'European Fruits'
+    _team: 'Fruits',
+    _measure: 2500000,
+    _ve: 700000,
+    _sbf: 0,
   },
   
   {
@@ -27,7 +33,10 @@ const arrAllProjects =
     _inpro: '56789',
     _invbm: 'inv',
     _country: 'Germany',
-    _team: 'European Fruits'
+    _team: 'Fruits',
+    _measure: 80000000,
+    _ve: 5000000,
+    _sbf: 50000,
   },
   
   {
@@ -37,9 +46,90 @@ const arrAllProjects =
     _inpro: '98765',
     _invbm: 'bm',
     _country: 'Germany',
-    _team: 'European Fruits'
+    _team: 'Fruits',
+    _measure: 1000000,
+    _ve: 200000,
+    _sbf: 5000,
   },
   
+  {
+    _name: 'Pflaume',
+    _eng: 'Plum',
+    _bmz: '20200110',
+    _inpro: '12346',
+    _invbm: 'inv',
+    _country: 'Germany',
+    _team: 'Fruits',
+    _measure: 12500000,
+    _ve: 200000,
+    _sbf: 15000,
+  },
+  
+  {
+    _name: 'Pflaume',
+    _eng: 'Plum',
+    _bmz: '20200110',
+    _inpro: '12346',
+    _invbm: 'bm',
+    _country: 'Germany',
+    _team: 'Fruits',
+    _measure: 100000,
+    _ve: 100000,
+    _sbf: 7000,
+  },
+
+  {
+    _name: 'Karotte',
+    _eng: 'Carrot',
+    _bmz: '2020010',
+    _inpro: '12350',
+    _invbm: 'inv',
+    _country: 'Germany',
+    _team: 'Vegetables',
+    _measure: 12000000,
+    _ve: 2500000,
+    _sbf: 35000,
+  },
+  
+  {
+    _name: 'Karotte',
+    _eng: 'Carrot',
+    _bmz: '20200310',
+    _inpro: '12349',
+    _invbm: 'bm',
+    _country: 'Germany',
+    _team: 'Vegetables',
+    _measure: 150000,
+    _ve: 1000000,
+    _sbf: 17000,
+  },
+
+  {
+    _name: 'Gurke',
+    _eng: 'Cucumber',
+    _bmz: '2020510',
+    _inpro: '12351',
+    _invbm: 'inv',
+    _country: 'Germany',
+    _team: 'Vegetables',
+    _measure: 12000000,
+    _ve: 2500000,
+    _sbf: 35000,
+  },
+  
+  {
+    _name: 'Gurke',
+    _eng: 'Cucumber',
+    _bmz: '20200310',
+    _inpro: '12349',
+    _invbm: 'bm',
+    _country: 'Germany',
+    _team: 'Vegetables',
+    _measure: 450000,
+    _ve: 120000,
+    _sbf: 11000,
+  },
+
   {
     _name: 'Mango',
     _eng: 'Mango',
@@ -47,7 +137,10 @@ const arrAllProjects =
     _inpro: '9876599',
     _invbm: 'inv',
     _country: 'India',
-    _team: 'Indian Fruits'
+    _team: 'Fruits',
+    _measure: 120000000,
+    _ve: 3000000,
+    _sbf: 25000,
   },
   
   {
@@ -57,8 +150,202 @@ const arrAllProjects =
     _inpro: '9876577',
     _invbm: 'bm',
     _country: 'India',
-    _team: 'Indian Fruits'
+    _team: 'Fruits',
+    _measure: 5000000,
+    _ve: 1500000,
+    _sbf: 10000,
+  },
+
+  {
+    _name: 'Papaya',
+    _eng: 'Papaya',
+    _bmz: '20200210',
+    _inpro: '12347',
+    _invbm: 'inv',
+    _country: 'India',
+    _team: 'Fruits',
+    _measure: 75000000,
+    _ve: 3500000,
+    _sbf: 12500,
+  },
+
+  {
+    _name: 'Papaya',
+    _eng: 'Papaya',
+    _bmz: '20200310',
+    _inpro: '12348',
+    _invbm: 'bm',
+    _country: 'India',
+    _team: 'Fruits',
+    _measure: 1500000,
+    _ve: 1000000,
+    _sbf: 35000,
+  },
+
+  {
+    _name: 'Okra',
+    _eng: 'Okra',
+    _bmz: '20200610',
+    _inpro: '12352',
+    _invbm: 'inv',
+    _country: 'India',
+    _team: 'Vegetables',
+    _measure: 110000000,
+    _ve: 4000000,
+    _sbf: 22500,
+  },
+
+  {
+    _name: 'Okra',
+    _eng: 'Okra',
+    _bmz: '20207310',
+    _inpro: '12354',
+    _invbm: 'bm',
+    _country: 'India',
+    _team: 'Vegetables',
+    _measure: 2500000,
+    _ve: 2000000,
+    _sbf: 12000,
+  },
+
+  {
+    _name: 'Meerrettichbaum',
+    _eng: 'Drumstick Tree',
+    _bmz: '20200810',
+    _inpro: '12355',
+    _invbm: 'inv',
+    _country: 'India',
+    _team: 'Vegetables',
+    _measure: 100000000,
+    _ve: 2000000,
+    _sbf: 1500,
+  },
+
+  {
+    _name: 'Meerrettichbaum',
+    _eng: 'Drumstick Tree',
+    _bmz: '20209310',
+    _inpro: '12356',
+    _invbm: 'bm',
+    _country: 'India',
+    _team: 'Vegetables',
+    _measure: 1000000,
+    _ve: 250000,
+    _sbf: 1000,
   }
   ]
   
+
+  const arrProjectDescriptions = [
+    {
+      _name: 'Apfel',
+      _desc: 'An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.',
+      _citation: 'https://en.wikipedia.org/wiki/Apple'
+    },
+    {
+      _name: 'Birne',
+      _desc: 'Pears are fruits produced and consumed around the world, growing on a tree and harvested in late Summer into October. The pear tree and shrub are a species of genus in the family Rosaceae, bearing the pomaceous fruit of the same name. Several species of pears are valued for their edible fruit and juices, while others are cultivated as trees. The tree is medium-sized and native to coastal and mildly temperate regions of Europe, North Africa, and Asia. Pear wood is one of the preferred materials in the manufacture of high-quality woodwind instruments and furniture. About 3000 known varieties of pears are grown worldwide, which vary in both shape and taste. The fruit is consumed fresh, canned, as juice, or dried.',
+      _citation: 'https://en.wikipedia.org/wiki/Pear'
+    },
+    {
+      _name: 'Pflaume',
+      _desc: 'A plum is a fruit of some species in Prunus subg. Prunus. Mature plum fruits may have a dusty-white waxy coating that gives them a glaucous appearance. This is an epicuticular wax coating and is known as "wax bloom". Dried plums are called prunes, which have a dark, wrinkled appearance.',
+      _citation: 'https://en.wikipedia.org/wiki/Plum',
+    },
+    {
+      _name: 'Karotte',
+      _desc: 'The carrot (Daucus carota subsp. sativus) is a root vegetable, usually orange in color, though purple, black, red, white, and yellow cultivars exist. They are a domesticated form of the wild carrot, Daucus carota, native to Europe and Southwestern Asia. The plant probably originated in Persia and was originally cultivated for its leaves and seeds. The most commonly eaten part of the plant is the taproot, although the stems and leaves are also eaten. The domestic carrot has been selectively bred for its greatly enlarged, more palatable, less woody-textured taproot.<br/>The carrot is a biennial plant in the umbellifer family, Apiaceae. At first, it grows a rosette of leaves while building up the enlarged taproot. Fast-growing cultivars mature within three months (90 days) of sowing the seed, while slower-maturing cultivars need a month longer (120 days). The roots contain high quantities of alpha- and beta-carotene, and are a good source of vitamin K and vitamin B6. <br/>The United Nations Food and Agriculture Organization (FAO) reports that world production of carrots and turnips (these plants are combined by the FAO) for 2018 was 40 million tonnes, with 45% of the world total grown in China. Carrots are widely used in many cuisines, especially in the preparation of salads, and carrot salads are a tradition in many regional cuisines.',
+      _citation: 'https://en.wikipedia.org/wiki/Carrot',
+    },
+    {
+      _name: 'Gurke',
+      _desc: 'Cucumber (Cucumis sativus) is a widely-cultivated creeping vine plant in the Cucurbitaceae gourd family that bears cucumiform fruits, which are used as vegetables. There are three main varieties of cucumber—slicing, pickling, and burpless/seedless—within which several cultivars have been created. The cucumber originates from South Asia, but now grows on most continents, as many different types of cucumber are traded on the global market. In North America, the term wild cucumber refers to plants in the genera Echinocystis and Marah, though the two are not closely related.',
+      _citation: 'https://en.wikipedia.org/wiki/Cucumber',
+    },
+    {
+      _name: 'Mango',
+      _desc: 'A mango is an edible stone fruit produced by the tropical tree Mangifera indica which is believed to have originated from the region between northwestern Myanmar, Bangladesh, and northeastern India. M. indica has been cultivated in South and Southeast Asia since ancient times resulting in two distinct types of modern mango cultivars: the "Indian type" and the "Southeast Asian type".Other species in the genus Mangifera also produce edible fruits that are also called "mangoes", the majority of which are found in the Malesian ecoregion.Worldwide, there are several hundred cultivars of mango. Depending on the cultivar, mango fruit varies in size, shape, sweetness, skin color, and flesh color which may be pale yellow, gold, or orange.Mango is the national fruit of India and Pakistan, and the national tree of Bangladesh.',
+      _citation: 'https://en.wikipedia.org/wiki/Mango',
+    },
+    {
+      _name: 'Papaya',
+      _desc: 'The papaya (/pəˈpaɪə/, US: /pəˈpɑːjə/) (from Carib via Spanish), papaw, (/pəˈpɔː/) or pawpaw (/ˈpɔːpɔː/) is the plant Carica papaya, one of the 22 accepted species in the genus Carica of the family Caricaceae. Its origin is in the tropics of the Americas, perhaps from Central America and southern Mexico.',
+      _citation: 'https://en.wikipedia.org/wiki/Papaya',
+    },
+    {
+      _name: 'Okra',
+      _desc: 'Okra or Okro (US: /ˈoʊkrə/, UK: /ˈɒkrə/), Abelmoschus esculentus, known in many English-speaking countries as ladies fingers or ochro, is a flowering plant in the mall family. It is valued for its edible green seed pods. It is a good source of minerals, vitamins, antioxidants, and fiber. The geographical origin of okra is disputed, with supporters of West African, Ethiopian, and South Asian origins. Traditionally, Okra is a southern U.S plant that thrives in relatively warmer temperatures. It is also easy to grow. As a result, the plant is cultivated in tropical, subtropical and warm temperate regions around the world.',
+      _citation: 'https://en.wikipedia.org/wiki/Okra',
+    },
+    {
+      _name: 'Meerrettichbaum',
+      _desc: 'Moringa oleifera is a fast-growing, drought-resistant tree of the family Moringaceae, native to the Indian subcontinent. Common names include moringa, drumstick tree (from the long, slender, triangular seed-pods), horseradish tree (from the taste of the roots, which resembles horseradish), and ben oil tree or benzolive tree. It is widely cultivated for its young seed pods and leaves, used as vegetables and for traditional herbal medicine. It is also used for water purification.Although listed as an invasive species in several countries, M. oleifera has "not been observed invading intact habitats or displacing native flora", so "should be regarded at present as a widely cultivated species with low invasive potential."',
+      _citation: 'https://en.wikipedia.org/wiki/Moringa_oleifera',
+    }
+  ]
    
+  const arrImages = [
+    {
+      _name: 'Apfel',
+      _source: './resources/images/apples.jpg',
+      _alt: 'Image of red apples',
+      _citationimg: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fvilee.fi%2Feng%2Fan-apple-a-day-health-benefits-of-apples%2F&psig=AOvVaw0tqXdA6ER6AwZ_MHAySV2P&ust=1620838739491000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCrnvyMwvACFQAAAAAdAAAAABAE',
+      _citationwp: 'https://vilee.fi/eng/an-apple-a-day-health-benefits-of-apples/',
+    },
+    {
+      _name: 'Birne',
+      _source: './resources/images/pears.jpg',
+      _alt: 'Image of three pears',
+      _citationimg: 'https://www.ndr.de/birne122_v-contentgross.jpg',
+      _citationwp: 'https://www.ndr.de/ratgeber/gesundheit/Birnen-suess-und-gesund,birnen107.html',
+    },
+    {
+      _name: 'Pflaume',
+      _source: './resources/images/plums.jpg',
+      _alt: 'Image of a plate of plums',
+      _citationimg: 'https://bilder.t-online.de/b/86/39/40/66/id_86394066/610/tid_da/zwetschgen-vor-dem-verzehr-sollten-sie-die-fruechte-waschen-.jpg',
+      _citationwp: 'https://www.t-online.de/leben/essen-und-trinken/id_84178558/zwetschgen-und-pflaumen-nicht-mitessen-das-ist-der-weisse-belag.html',
+    },
+    {
+      _name: 'Karotte',
+      _source: "./resources/images/carrots.jpg",
+      _alt: 'Image of some carrots',
+      _citationimg: 'https://static.openfoodfacts.org/images/products/501/025/174/4627/front_en.3.full.jpg',
+      _citationwp: 'https://fr-en.openfoodfacts.org/product/5010251744627/carrots',
+    },
+    {
+      _name: 'Gurke',
+      _source: './resources/images/cucumbers.jpg',
+      _alt: 'Image of some green cucumbers',
+      _citationimg: 'https://cdn.pixabay.com/photo/2018/02/04/04/43/vegetable-3129062__340.jpg',
+      _citationwp: 'https://pixabay.com/de/photos/search/cucumber/',
+    },
+    {
+      _name: 'Mango',
+      _source: './resources/images/mango.jpg',
+      _alt: 'Upfront image of a mango',
+      _citationimg: 'https://upload.wikimedia.org/wikipedia/commons/4/40/Mango_4.jpg',
+      _citationwp: 'https://commons.wikimedia.org/wiki/File:Mango_4.jpg',
+    },
+    {
+      _name: 'Papaya',
+      _source: './resources/images/papaya.jpg',
+      _alt: 'Image of two cut papayas',
+      _citationimg: 'https://exoticfruitbox.com/wp-content/uploads/2015/10/papaya-3-Exotic-500x500.jpg',
+      _citationwp: 'https://exoticfruitbox.com/de/tropical-fruits/papaya/',
+    },
+    {
+      _name: 'Okra',
+      _source: './resources/images/okra.jpg',
+      _alt: 'Image of okra cut into peaces',
+      _citationimg: 'https://cdn.pixabay.com/photo/2018/05/09/02/24/okra-3384427__340.jpg',
+      _citationwp: 'https://pixabay.com/de/photos/search/ladyfinger/',
+    },
+    {
+      _name: 'Meerrettichbaum',
+      _source: './resources/images/drumstick.jpg',
+      _alt: 'Image of a few indian drumsticks (vegetable)',
+      _citationimg: 'https://www.magicgardenseeds.de/pics/mgs/1311/1000/Meerrettichbaum-(Moringa-oleifera).jpg',
+      _citationwp: 'https://www.magicgardenseeds.de/Wissenswertes/Meerrettichbaum-(Moringa-oleifera)-A.1311-',
+    }
+  ]
